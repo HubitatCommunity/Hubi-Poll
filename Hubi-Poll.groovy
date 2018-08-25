0dfcae2ccf9308d25c9f20670317f5a411434170
c steele <csteele@pandoradigital.com>
2018-08-24 23:33:35 -0700

    Update Hubi-Poll.groovy
    version.json at github.io (github pages)

--- Hubi-Poll.groovy
+++ Hubi-Poll.groovy
@@ -140,7 +140,7 @@ def display(){
 def updatecheck(){
     
     setAppVersion()
-    def paramsUD = [uri: "http://u-qk.com/versions.json"]
+    def paramsUD = [uri: "https://hubitatcommunity.github.io/Hubi-Poll/versions.json"]
        try {
         httpGet(paramsUD) { respUD ->
  //  log.info " Version Checking - Response Data: ${respUD.data}"
